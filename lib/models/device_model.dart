8e1efb800ba57ddd952247319005f1d0d7174d7b
Zeynat <zeynat.haytham.ghallab@gmail.com>
2025-10-08 14:51:48 +0300

    Voice command and Device Model Implementation

---
+++ lib/models/device_model.dart
@@ -0,0 +1,15 @@
+class DeviceModel {
+  final String id;
+  final String name;
+  final String status;
+  final String distance;
+  final int batteryLevel;
+
+  DeviceModel({
+    required this.id,
+    required this.name,
+    required this.status,
+    required this.distance,
+    required this.batteryLevel,
+  });
+}
---
+++ lib/screens/network_dashboard.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import '../widgets/voice_command_button.dart';
+import '../models/device_model.dart';
+
