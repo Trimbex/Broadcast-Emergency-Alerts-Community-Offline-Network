f8542dd19b2efb05c5492aa77aa18f2b48a1913b
Trimbex <111075379+Trimbex@users.noreply.github.com>
2025-10-18 19:58:26 +0300

    added message model
    still errors in chatpage

---
+++ lib/screens/chat_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import '../widgets/voice_command_button.dart';
+import '../models/device_model.dart';
+import '../models/message_model.dart';
+
+class ChatPage extends StatefulWidget {
+  const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
